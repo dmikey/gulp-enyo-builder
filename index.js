b1d2e36aa3943202ec50e0caa4e18b8f0aabf783
Derek M. Anderson <derek.anderson@coxinc.com>
2014-05-30 00:33:54 -0500

    commiting base files

---
+++ index.js
@@ -0,0 +1,41 @@
+'use strict';
+var fs = require('fs');
+var path = require('path');
+var git = require('gulp-git');
+var modulepath = path.resolve('node_modules/gulp-enyo-builder');
+var enyo = modulepath + '/enyo';
+var exec = require('child_process').exec;
+var sys = require('sys')
+
+module.exports = function (opts) {
+    opts = opts || {};
+    opts.cwd = opts.cwd || process.cwd();
+    opts.tag = opts.tag || '2.4.0';
+
+    try {
+        //see if enyo has been cloned
+        var files = fs.readdirSync(enyo);
+    } catch (e) {
+        //clone enyo
+        git.clone('https://github.com/enyojs/enyo.git', {
+            args: enyo
+        });
+
+        git.checkout(opts.tag);
+    }
+
+    var deploypath = enyo + '/tools/deploy.js';
+    var buildCmd = ['nodejs ', deploypath, ' -T -e ', enyo, ' -s ', process.cwd(), ' -o ', process.cwd(), '/dist -l ', opts.lib];
+    var child = exec(buildCmd.join(''), function (error, stdout, stderr) {
+
+        sys.print('stdout: ' + stdout);
+        sys.print('stderr: ' + stderr);
+
+        if (error !== null) {
+
+            console.log('exec error: ' + error);
+        }
+
+    });
+
+};
